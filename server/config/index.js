792a3de596569535bcdb2c4fa1daad3f8722623b
Eric Nyirimana <ericnyirimana31@gmail.com>
2019-02-10 18:12:48 +0200

    [Chore #163843906] implementing node JS

---
+++ server/config/index.js
@@ -0,0 +1,9 @@
+import app from '../app';
+
+const port = process.env.PORT || '3000';
+
+const listen = () => {
+  console.log(`Running on ${port}`);
+};
+
+app.listen(port, listen());
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+// import users from './users';
+
+const app = express();
+
+
+// app.use('/users', users);
+
+export default app;
